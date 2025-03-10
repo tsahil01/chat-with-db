28f9e3366a2a736cd566054090660aba58893d8b
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-03-10 20:22:44 +0530

    get schema done

--- backend/src/chat.ts
+++ backend/src/chat.ts
@@ -4,6 +4,7 @@ import { Message } from "./types";
 
 const client = new OpenAI({
     apiKey: process.env['API_KEY'],
+    baseURL: 'https://openrouter.ai/api/v1'
 });
 
 export async function chat(messages: Message[], newMessage: Message) {
@@ -14,15 +15,15 @@ export async function chat(messages: Message[], newMessage: Message) {
 
     const stream = await client.chat.completions.create({
         messages: messages,
-        model: '<model-id>',
+        model: 'cognitivecomputations/dolphin3.0-r1-mistral-24b:free',
         stream: true
     });
 
     let fullResponse = '';
     for await (const chunk of stream) {
         const data = chunk.choices[0]?.delta?.content || '';
-        fullResponse += data;
         process.stdout.write(data);
+        fullResponse += data;
     }
 
     return fullResponse;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,58 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+dotenv.config();
 
 const app = express();
 const port = 3000;
+app.use(express.json());
+const tablesQuery = `
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public';
+`;
+
+const columnsQuery = `
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = $1;
+`;
+
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
+app.post('/chat', (req, res) => {
+    const { messages, prompt } = req.body;
+    res.send('Chat message received!');
+});
+
+app.post('/schema', async (req, res) => {
+    const { DB_URL } = req.body;
+    try {
+        const pool = await new Pool({
+            connectionString: DB_URL
+        });
+        console.log("Connected to database!");
+
+        const tables = await pool.query(tablesQuery);
+
+        const schema: { [key: string]: any } = {};
+
+        for (const table of tables.rows) {
+            const tableName = table.table_name;
+            const columns = await pool.query(columnsQuery, [tableName]);
+            schema[tableName] = columns.rows;
+        }
+        res.json(schema);
+    } catch (error) {
+        console.error("error: ", error);
+    }
+
+})
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
